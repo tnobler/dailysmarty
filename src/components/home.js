49d5cf7ff414337f90e3c4d2d5c6ecc309584524
Tanner Noble <tannernoble@Tanners-iMac.local>
2019-12-16 14:16:41 -0600

    setup app routes

--- src/components/home.js
+++ src/components/home.js
@@ -4,10 +4,10 @@ import Logo from './logo';
 import SearchBar from './searchBar';
 import RecentPosts from './recentPosts';
 
-export default class App extends Component {
+export default class Home extends Component {
   render() {
     return (
-      <div className='app'>
+      <div>
         <div>
           <Logo />
           <SearchBar />
