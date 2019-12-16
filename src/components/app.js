abf56d7af1f345c2e1a30563e1bd5ef2109284dc
Tanner Noble <tannernoble@Tanners-iMac.local>
2019-12-16 13:51:40 -0600

    setup logo component and corresponding styles

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Logo from './logo';
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <div>Logo</div>
+        <div>
+          <Logo />
+        </div>
       </div>
     );
   }
